cc0346fa1d6f5645c7c55f1af698b35da97fc106
Trần Minh Duy <tranminhduy1902@gmail.com>
2025-10-24 21:01:22 +0700

    replace 1

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -92,19 +92,6 @@ class ProductController {
     }
   }
 
-  async getProductById(req, res) {
-    try {
-      const { id } = req.params;
-      const product = await this.productService.getProductById(id);
-      if (!product) {
-        return res.status(404).json({ message: "Product not found" });
-      }
-      res.status(200).json(product);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Server error" });
-    }
-  }
 
 }
 
