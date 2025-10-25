ebf290c5de3d8b13838b7bf68409c30aa446776c
Trần Minh Duy <tranminhduy1902@gmail.com>
2025-10-25 09:05:00 +0700

    test

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -7,7 +7,7 @@ class ProductController {
     this.getProducts = this.getProducts.bind(this);
     this.createOrder = this.createOrder.bind(this);
     this.getOrderById = this.getOrderById.bind(this);
-    this.getProductsById = this.getProductsById.bind(this);
+    this.getProductById = this.getProductById.bind(this);
   }
   async createProduct(req, res) {
     try {
@@ -91,14 +91,14 @@ class ProductController {
       res.status(500).json({ message: "Server error" });
     }
   }
-  async getProductsById(req, res){
+  async getProductById(req, res) {
     try {
       const { id } = req.params;
-      const products = await this.productService.getProductById(id);
-      if(!products){
-        return res.status(404).json({ message: "khong tim thay sp" });
+      const product = await this.productService.getProductById(id);
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
       }
-      res.status(200).json(products);
+      res.status(200).json(product);
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Server error" });
